0b47305b7a6491b0630dfdec95b21c11d3d9c8b0
Yana <yana.misyuchenko.94@mail.ru>
2016-12-22 23:12:32 +0300

    First commit

---
+++ Chip Test/src/readcontroller/Globals.java
@@ -0,0 +1,19 @@
+package readcontroller;
+
+public final class Globals {
+
+	private MainFrame mf;
+
+
+
+	public void setMainFrame(MainFrame mf) {
+		this.mf = mf;
+	}
+
+
+
+	public MainFrame getMainFrame() {
+		return mf;
+	}
+
+}
---
+++ Chip Test/src/readcontroller/SerialCallback.java
@@ -0,0 +1,7 @@
+package readcontroller;
+
+public interface SerialCallback {
+
+	public void onProcessed(byte[] buffer, int code);
+	
+}
